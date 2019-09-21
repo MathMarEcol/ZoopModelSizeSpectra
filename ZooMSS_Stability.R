f0303a4ffcada372741a1a33e8c67fb47fccf052
patricksykes <52962150+patricksykes@users.noreply.github.com>
2019-09-21 20:16:16 +1000

    Add files via upload

--- ZooMSS_Stability.R
+++ ZooMSS_Stability.R
@@ -4,14 +4,12 @@
 
 ########## NEWTON RAPHSON
 ## du.calc:
-## u_initial is the starting size spectrum, z.b is an internal indicator
-## specific to the model, it indicates whether the function should use an
-## m-value to calculate zoo PPMR, or just use a fixed PPMR
+## u_initial is the starting size spectrum
 
-du.calc <- function(u_initial, z.b){
+du.calc <- function(u_initial){
 # Run the PZF model, output is a matrix of size spectrum for each time step
 # the model is run (usually just N = 2, so only 2 size spectra)
-out <- PZF.solve(state = u_initial, parms = params, test = 0)[1]
+out <- ZooMSS_PDE(state = u_initial, parms = params, test = 0)[1]
 
 # Take the last time step's size spectrum as the new size spectrum to calculate
 # du for each size class
@@ -24,7 +22,7 @@ return(du)
 }
 
 ## Calculate Jacobian Matrix
-jac.calc <- function(u.init, du, z.b){
+jac.calc <- function(u.init, du){
 	jac = matrix(0, length(du), length(du))
 	
 	for(j in 1:length(du)){
@@ -41,7 +39,7 @@ jac.calc <- function(u.init, du, z.b){
 
  	  h=u_temp[j]-temp  #Error rounding check step
 
-    du.new <- du.calc(u_temp, z.b) #Calculate new rhs values
+    du.new <- du.calc(u_temp) #Calculate new rhs values
 
     u_temp[j]=temp #Restore variable value
 
@@ -52,17 +50,16 @@ jac.calc <- function(u.init, du, z.b){
 
 
 
-Newton <- function(u_initial, ntrials = 20, tol = 1e-4, z.b){
+Newton <- function(u_initial, ntrials = 20, tol = 1e-4){
 	u_temp<-u_initial
-	z.b = z.b
-  	erf = 1 # Starting value for convergence function
-  	i = 0
+  erf = 1 # Starting value for convergence function
+  i = 0
   while(erf > tol && i < ntrials){ # This runs until convergence
                                    # or we run out of trials
     
-    du <- du.calc(u_temp, z.b) ## Calculate current du's
+    du <- du.calc(u_temp) ## Calculate current du's
 
-    jac.c <- jac.calc(u_temp, du, z.b) ## Calculate current Jacobian
+    jac.c <- jac.calc(u_temp, du) ## Calculate current Jacobian
 
     u_temp <- as.vector(u_temp + solve(jac.c,-du, tol=1e-25)) # Update u_temp
     
